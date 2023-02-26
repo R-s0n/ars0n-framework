a57e075d98fafd973b0637d0c96c4b1b098d9698
Harrison Richardson <rs0n.evolv3@gmail.com>
2023-02-26 13:12:41 -0800

    fix bootstrap bug

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import './custom.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
