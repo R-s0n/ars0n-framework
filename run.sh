de157411db79c51a2fbf54f60e8952f8a39e4b65
deepakvamsi <34811671+deepakvamsi@users.noreply.github.com>
2023-11-08 16:55:21 +0530

    Update run.sh
    Wrong spelling in the run.sh.
    \\Changed mongodb to mongod

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo systemctl enable mongodb 2>/dev/null
-sudo systemctl start mongodb 2>/dev/null
+sudo systemctl enable mongod 2>/dev/null
+sudo systemctl start mongod 2>/dev/null
 node server/server.js &
 cd client
 npm run start &
