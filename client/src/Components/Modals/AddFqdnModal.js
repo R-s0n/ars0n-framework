8ec22fc8191a03d86868836843788637593823ad
demo <demo@ars0n-framework.rs0n-framework.xyz>
2023-12-08 18:36:34 -0600

    Add framework scan file upload to add fqdn modal

--- client/src/Components/Modals/AddFqdnModal.js
+++ client/src/Components/Modals/AddFqdnModal.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 
 const AddFqdnModal = props => {
     const [file, setFile] = useState(null);
+    const [scanFile, setScanFile] = useState(null);
     const [manualFqdn, setManualFqdn] = useState("");
 
     const handleFileChange = (e) => {
@@ -11,6 +12,10 @@ const AddFqdnModal = props => {
 
     const handleManualChange = (e) => {
         setManualFqdn(e.target.value);
+    }
+
+    const handleScanFileChange = (e) => {
+        setScanFile(e.target.files[0]);
     }
 
     const handleSubmit = (e) => {
@@ -24,25 +29,18 @@ const AddFqdnModal = props => {
                     props.setNoFqdns(false); 
                     const content = JSON.parse(e.target.result);
                     const inScope = content.target?.scope?.include;
-            
                     if (Array.isArray(inScope)) {
-                        // Create an initial list of domains
                         let domains = inScope.filter(inclusion => inclusion.enabled)
                                              .map(inclusion => {
-                                                 // Remove protocol, www, and any special regex characters
                                                  return inclusion.host.replace(/(https?:\/\/)?(www\.)?/g, '')
                                                                       .replace(/[\^$\\]/g, '');
                                              });
     
-                        // Filter out duplicates
                         domains = [...new Set(domains)];
-    
                         axios.all(domains.map(domain => axios.post('http://localhost:8000/api/fqdn/new', { fqdn: domain })))
                         .then(axios.spread((...responses) => {
                             const newFqdns = responses.map(res => res.data);
                             props.setFqdns(prevFqdns => [...prevFqdns, ...newFqdns]);
-            
-                            // Set the first FQDN from the new list as the current FQDN
                             if (newFqdns.length > 0) {
                                 props.setCurrentFqdn(newFqdns[0]);
                             }
@@ -58,16 +56,45 @@ const AddFqdnModal = props => {
         } else if (manualFqdn) {
             console.log("Manually adding...");
             props.setNoFqdns(false); 
-            // Handle manual domain addition
             const domain = manualFqdn.replace(/(https?:\/\/)?(www\.)?/g, '');
             axios.post('http://localhost:8000/api/fqdn/new', { fqdn: domain })
                 .then(res => {
                     const newFqdn = res.data;
                     props.setFqdns(prevFqdns => [...prevFqdns, newFqdn]);
-                    props.setNoFqdns(false); // Call this function to close the modal
+                    props.setNoFqdns(false); 
                 })
                 .catch(err => console.log(err));
-        }
+        } else if (scanFile) {
+            console.log(scanFile);
+            console.log("Loading Scan File...")
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                try {
+                    const importedData = JSON.parse(e.target.result);
+                    importedData.forEach((importedFqdn) => {
+                        const existingIndex = props.fqdns.findIndex(
+                        (existingFqdn) => existingFqdn.fqdn === importedFqdn.fqdn
+                        );
+                        if (existingIndex === -1) {
+                            props.setFqdns((prevData) => [...prevData, importedFqdn]);
+                            axios.post("http://localhost:8000/api/fqdn/new",importedFqdn)
+                        } else {
+                            props.setFqdns((prevData) => {
+                                const newData = [...prevData];
+                                newData[existingIndex] = importedFqdn;
+                                return newData;
+                            });
+                            axios.post("http://localhost:8000/api/fqdn/update",importedFqdn)
+                        }
+                    });
+                    props.setNoFqdns(false);
+                } catch (error) {
+                    console.error('Error parsing JSON file:', error);
+                }
+            };
+            reader.readAsText(scanFile);
+            props.setNoFqdns(false);
+            }
     }        
     
     
@@ -80,6 +107,10 @@ const AddFqdnModal = props => {
                 <div>
                     <label>Config File:</label>
                     <input type="file" onChange={handleFileChange} />
+                </div>
+                <div>
+                    <label>Ars0n Framework Scan File:</label>
+                    <input type="file" onChange={handleScanFileChange} />
                 </div>
                 <div>
                     <label>Manual FQDN:</label>
