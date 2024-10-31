1f0b191d5e4c1dacd5be7b264be3175656dfa64a
R-s0n <rs0n.evolv3@gmail.com>
2024-10-30 18:22:55 -0700

    crossing fingers and running first scan...

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://root:example@mongodb:27017/wapt_db?authSource=admin')
+mongoose.connect('mongodb://root:example@mongodb:27017/wapt_db?authSource=admin',{ useNewUrlParser: true })
     .then(()=>console.log("Database connection established..."))
     .catch(err=>console.log("ERROR: ", err));
